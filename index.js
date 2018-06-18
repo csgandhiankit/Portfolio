63587179bf8fceef659bf880b446992ca65e5c92
Ankit Gandhi <csgandhiankit@gmail.com>
2018-06-18 12:41:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/* eslint-env browser  */
+
+(() => {
+  const buttonRow = document.querySelector('.button-row');
+  const typed = document.querySelector('.typed');
+
+  const additionalStrings = [
+    'Software Engineer',
+    'Web Developer',
+    'Bird Watcher',
+    'Coffee Lover',
+  ];
+  const lastLoadedTime = parseInt(localStorage.getItem('lastLoaded'), 10) || 0;
+  const lastLoaded = new Date(lastLoadedTime);
+  const now = new Date();
+  
+  const shouldShowStrings = lastLoaded.getYear() < now.getYear()
+      || lastLoaded.getMonth() < now.getMonth();
+
+  new Typed('.typed .text', {
+    backSpeed: 30,
+    onComplete() {
+      buttonRow.classList.remove('hidden');
+      typed.classList.remove('before-buttons');
+      if (shouldShowStrings) {
+        localStorage.setItem('lastLoaded', Date.now());
+      }
+    },
+    showCursor: false,
+    strings: [].concat(
+      shouldShowStrings ? additionalStrings : [],
+      'Ankit Gandhi',
+    ),
+    typeSpeed: 30,
+  });
+
+  mdc.autoInit();
+})();
